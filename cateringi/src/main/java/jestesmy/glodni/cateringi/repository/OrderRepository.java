3027f20d8ea8059d174a320188edd0d2e2a75ec5
cez.heldt <cezary.heldt@gmail.com>
2020-01-14 10:16:23 +0100

    merge company profile

--- cateringi/src/main/java/jestesmy/glodni/cateringi/repository/OrderRepository.java
+++ cateringi/src/main/java/jestesmy/glodni/cateringi/repository/OrderRepository.java
@@ -1,7 +1,9 @@
 package jestesmy.glodni.cateringi.repository;
 
-import jestesmy.glodni.cateringi.model.Client;
-import jestesmy.glodni.cateringi.model.Order;
+import jestesmy.glodni.cateringi.domain.model.Client;
+import jestesmy.glodni.cateringi.domain.model.Company;
+import jestesmy.glodni.cateringi.domain.model.Order;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
