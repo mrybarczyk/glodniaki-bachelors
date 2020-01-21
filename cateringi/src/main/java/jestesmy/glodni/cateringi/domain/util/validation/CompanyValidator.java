b7abe875e4c8e8c9f51064bf6a8ae32a44f90d67
cez.heldt <cezary.heldt@gmail.com>
2020-01-21 13:35:56 +0100

    nip regon validator

---
+++ cateringi/src/main/java/jestesmy/glodni/cateringi/domain/util/validation/CompanyValidator.java
@@ -0,0 +1,58 @@
+package jestesmy.glodni.cateringi.domain.util.validation;
+
+public class CompanyValidator {
+
+    private static String validateRegon(String regon) {
+        int[] weights;
+        if(regon.matches("\\d*")) {
+            if (regon.length() == 9) {
+                weights = new int[] {8, 9, 2, 3, 4, 5, 6, 7};
+            } else if (regon.length() == 14) {
+                weights = new int[] {2, 4, 8, 5, 0, 9, 7, 3, 6, 1, 2, 4, 8 };
+            } else
+                return "REGON powinien mieć 9 lub 14 cyfr";
+        } else
+            return "REGON powinien składać się z samych cyfr";
+        int checkSum=0;
+        for(int i=0;i<weights.length;i++) {
+            checkSum+= weights[i]*Integer.parseInt(String.valueOf(regon.charAt(i)));
+        }
+        checkSum %= 11;
+        String lastDigit = String.valueOf(regon.charAt(regon.length()-1));
+        if(checkSum==10) {
+            if(lastDigit.equals("0"))
+                return "";
+            else {
+                return "REGON jest niepoprawny";
+            }
+        } else {
+            if(Integer.parseInt(lastDigit)==checkSum)
+                return "";
+            else
+                return "REGON jest niepoprawny";
+        }
+    }
+
+    private static String validateNip(String nip) {
+        int[] weights = {6, 5, 7, 2, 3, 4, 5, 6, 7};
+        if(nip.matches("\\d*")){
+            if(nip.length()!=10)
+                return "NIP powinien mieć 10 cyfr";
+        } else
+            return "NIP powinien składać się z samych cyfr";
+        int checkSum = 0;
+        for(int i=0;i<weights.length;i++) {
+            checkSum += weights[i]* Integer.parseInt(String.valueOf(nip.charAt(i)));
+        }
+        checkSum %= 11;
+        int lastDigit = Integer.parseInt(String.valueOf(nip.charAt(9)));
+        if(lastDigit!=checkSum)
+            return "NIP niepoprawny";
+        return "";
+    }
+
+    public static void main (String[] args) {
+        System.out.println(validateRegon("12345678512348"));
+    }
+
+}
