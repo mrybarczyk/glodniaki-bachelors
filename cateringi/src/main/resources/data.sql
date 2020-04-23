eb5d955faed10abc42341b679ffb56d44fdfe533
cez.heldt <cezary.heldt@gmail.com>
2020-04-23 19:51:37 +0200

    update branch and change admin side menu

--- cateringi/src/main/resources/data.sql
+++ cateringi/src/main/resources/data.sql
@@ -4,5 +4,5 @@ INSERT INTO Categories(categoryID,name) VALUES (3,'Keto');
 INSERT INTO Categories(categoryID,name) VALUES (4,'Fit');
 INSERT INTO Categories(categoryID,name) VALUES (5,'Inne');
 
-INSERT INTO Users(userId, email, isActive, password, phoneNumber, userName, userType) values (1,'admin@admin.admin',true,'21232f297a57a5a743894a0e4a801fc3','123123123','admin','ADMIN');
+INSERT INTO Users(userId, email, isActive, password, phoneNumber, userName, userType, messageCounter) values (1,'admin@admin.admin',true,'21232f297a57a5a743894a0e4a801fc3','123123123','admin','ADMIN',0);
 INSERT INTO Admins(lastName, name, adminID) values ('admin','admin',1);
